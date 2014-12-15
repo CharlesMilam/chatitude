ee3ddfd1cee65c18530bd22dc736a02fe10bb0d7
charles milam <charles.milam@gmail.com>
2014-12-14 22:53:57 -0600

    Modify

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-$(function  () {
-  var chatsData = [{user:"Nike", message:199.00 }, {user:"Loafers", message:59.00 }, {user:"Wing Tip", message:259.00 }];
- //Get the HTML from the template   in the script tag​
-  var source = $("#chats-template").html(); 
-​
- //Compile the template​
-  var template​ = Handlebars.compile(source); 
-  $('.chatsList').append(template​(chatsData)); 
-​
+$(function () {
+  var chatsData = [{user:"scruffy", message:"this is a test."}];
+  var source = $('#chats-template').html();
+  var getChatsTemplate​ = Handlebars.compile(source);
+
+  $('.chatsList').append(getChatsTemplate​(chatsData));
 });
