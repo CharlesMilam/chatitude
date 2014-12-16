51cc81899fec7905a9674b25e23c8af762ec5432
charles milam <charles.milam@gmail.com>
2014-12-15 21:40:22 -0600

    Add alert, hide and clear inputs.

---
+++ public/js/signin.js
@@ -0,0 +1,24 @@
+function ajaxSignin() {
+  $("#signinBtn").click(function() {
+    console.log("in click")
+    $.ajax({
+      type: 'POST',
+      url: 'http://chat.api.mks.io/signin',
+      data: {
+      username: $("#usernameSignin").val(),
+      password: $("#passwordSignin").val()
+      }
+      
+    }).success(function (token) {
+      console.log("signin success", token)
+      sessionStorage.setItem("apiToken", token["apiToken"])
+      // inform user of successful signup, hide and clear inputs.
+      alert($("#usernameSignin").val() + " successfully signed in.");
+      $(".signinContainer").hide();
+      $("#usernameSignin").val('');
+      $("#passwordSignin").val('');
+    })
+  })
+};
+
+ajaxSignin();
