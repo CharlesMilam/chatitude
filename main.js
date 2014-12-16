6b842f448d527f35498249635b1348f2db4551ac
Brian Atwood <batwood011@gmail.com>
2014-12-16 12:07:20 -0600

    clean up root directory

--- main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  var chatsData = [{user:"scruffy", message:"this is a test."}];
-  var source = $('#chats-template').html();
-  var getChatsTemplate​ = Handlebars.compile(source);
-
-  $('.chatsList').append(getChatsTemplate​(chatsData));
-});
